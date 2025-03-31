6a33396da4a772ea6aac9de8b547de17ccf81f28
Yuvashree C <yuvashreecv@gmail.com>
2025-03-31 13:19:59 +0530

    ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -148,13 +148,6 @@ const ContactSection: React.FC = () => {
                         <path d="M9 18c-4.51 2-5-2-7-2"/>
                       </svg>
                     </a>
-                    <a href="https:/www.instagram.com/glowingstar_2136" className="w-10 h-10 bg-lilac/20 rounded-full flex items-center justify-center hover:bg-lilac/40 transition-colors">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-lilac">
-                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>
-                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
-                        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
-                      </svg>
-                    </a>
                   </div>
                 </div>
               </AnimatedCard>
